18f2d57d762f9ecdc22ef7401776366efd7942ee
Dan Hancox <dthancox@gmail.com>
2022-10-21 10:57:59 +0100

    walkie walkie

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -11,4 +11,52 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
     });
+
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+
+    it('has a initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it('has a initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
 });
+
+
+describe('Your pet is growing up', () => {
+        
+    it('pet age up by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    })
+    it('pet hunger up by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    })
+    it('pet fitness down by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    })
+})
+
+describe('exercise your pet, take for a walk', () => {
+    it('pet fitness increased but never over 10', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);      
+        pet.walk();
+        expect(pet.fitness).toEqual(10)
+    })
+})
+
+
+
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,44 @@
+const maximumFitness = 10;
+const minimumHunger = 0;
+const minimumFitness = 0;
+const lengthOfExistance = 30;
+const startingAge = 0;
+
+//Creating the constructor for Pet
 function Pet(name){
-    this.name = name;
+    this.name = name
+    this.age = startingAge
+    this.hunger = minimumHunger
+    this.fitness = maximumFitness
 }
+
+//create variables to use rather than writing repetitive numbers in the objects. these const variables have global scope
+const aging = 1;
+const snack = 5;
+const fatigue = 3;
+//Pet growing older, higher age and hunger, less fitness
+Pet.prototype.growUp = function() {
+    this.age += aging;
+    this.hunger += snack;
+    this.fitness -= fatigue;
+}
+
+const walkies = 4;
+//Elevates the level of your pets fitness by 4, unless already maxed fitness
+Pet.prototype.walk = function () { 
+    this.fitness += walkies;
+        if(this.fitness > maximumFitness) {
+        this.fitness = maximumFitness;
+    }};
+
+
+
+    
+    
+        
+        
+
+
+
 
 module.exports = Pet;
